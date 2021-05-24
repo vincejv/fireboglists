79043347fb039eb0f123458533a04edc491d82b5
Vince Jerald Villamora <vincevillamora@gmail.com>
2021-05-24 12:39:04 +0000

    Update tick list for Mon 24 May 2021 12:36:09 PM UTC

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Firebog Tick List
-Last updated: Mon 24 May 2021 12:20:11 PM UTC
+Last updated: Mon 24 May 2021 12:36:09 PM UTC
--- populate.sh
+++ populate.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-AUTHOR_NAME="Vince Jerald Villamora"
-AUTHOR_EMAIL="vincevillamora@gmail.com"
-USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+AUTHOR_NAME="Vince Jerald Villamora"             # Author name
+AUTHOR_EMAIL="vincevillamora@gmail.com"          # Author email
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"  # Custom user agent string to prevent detection
 LAST_UPD=$(date -u)
-GIT_CREDS=$(cat /opt/scripts/ads/.gittoken)
+GIT_CREDS=$(cat /opt/scripts/ads/.gittoken)      # Git token in the format of ${USERNAME}:${TOKEN/PASSWORD}
+GIT_PROT="https"                                 # Git Protocol to use
+GIT_URL="github.com/vincejv/fireboglists"        # Destination repo, without protocol specified
 
 echo "Setting up git credentials"
-git config user.name "$AUTHOR_NAME"
-git config user.email "$AUTHOR_EMAIL"
+/usr/bin/git config user.name "$AUTHOR_NAME"
+/usr/bin/git config user.email "$AUTHOR_EMAIL"
 
 echo "Cleaning up before start"
 rm -rf target/
@@ -16,35 +18,35 @@ rm -rf working/
 mkdir target/
 mkdir working/
 
-systemd-notify --ready --status="Downloading from sources"
+/usr/bin/systemd-notify --ready --status="Downloading from sources"
 
 echo "Downloading blocklist source"
-curl https://v.firebog.net/hosts/lists.php?type=tick > working/fireboglist.txt
+/usr/bin/curl --user-agent "$USER_AGENT" https://v.firebog.net/hosts/lists.php?type=tick > working/fireboglist.txt
 echo "Downloading blocklist"
-wget -nv -U "$USER_AGENT" -i working/fireboglist.txt -P target/
+/usr/bin/wget -nv -U "$USER_AGENT" -i working/fireboglist.txt -P target/
 echo "Combining lists"
 cat target/* > working/combinedlist.txt
 echo "Sorting and removing duplicates"
-sort working/combinedlist.txt | uniq > ticklist
+/usr/bin/sort working/combinedlist.txt | /usr/bin/uniq > ticklist
 
 echo "Cleaning up repository before commit"
 rm -rf target/
 rm -rf working/
 
-systemd-notify --status="Processing blocklists"
+/usr/bin/systemd-notify --status="Processing blocklists"
 
 echo "# Firebog Tick List" > README.md
 echo "Last updated: $LAST_UPD" >> README.md
 echo "Commiting to repository"
-git add -A .
-git commit -m "Update tick list for ${LAST_UPD}"
+/usr/bin/git add -A .
+/usr/bin/git commit -m "Update tick list for ${LAST_UPD}"
 echo "Pushing to repository"
-git push "https://${GIT_CREDS}@github.com/vincejv/fireboglists"
+/usr/bin/git push "${GIT_PROT}://${GIT_CREDS}@{GIT_URL}"
 
 echo "Wait for git cache..."
-sleep 5
+/usr/bin/sleep 5
 echo "Updating pihole lists"
 
-systemd-notify --status="Updating pihole blocklists"
-pihole -g
+/usr/bin/systemd-notify --status="Updating pihole blocklists"
+/usr/local/bin/pihole -g
 echo "Done!"
