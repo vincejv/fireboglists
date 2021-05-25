696b3a433807c89f49fa44d0d35b501d6d98c600
Vince Jerald Villamora <vincevillamora@gmail.com>
2021-05-25 14:45:03 +0000

    Updated script to only sync with repo when gravity wasn't updated recently

--- populate.sh
+++ populate.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
+GRAVITY_DB="/etc/pihole/gravity.db"
+GIT_CREDS_FILE="/opt/scripts/ads/.gittoken"
+
+# check if files exist
+prevalidation() {
+  if [ ! -f "$GRAVITY_DB" ] || [ ! -f "$GIT_CREDS_FILE" ]; then
+    echo "Check if ${GRAVITY_DB} and ${GIT_CRED_FILE} exists" 1>&2
+    exit 1 # terminate and indicate error
+  fi
+}
+
+prevalidation
+
+# Wget options
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"  # Custom user agent string to prevent detection
+DELAY_WGET="4"                                    # Add wait to prevent scraping detection
+
+# Gravity lists options
+LAST_UPD=$(date -u)                                       # Full UTC Date
+DAY_TO_CHECK="7"
+DAYS_AGO=$(date -u -d "now - ${DAYS_TO_CHECK} days" +%s)  # Max Last gravity update
+GRAVITY_LAST_UPD=$(date -u -r "$GRAVITY_DB" +%s)          # Gravity DB Last update in UTC
+
+# Git options
 AUTHOR_NAME="Vince Jerald Villamora"             # Author name
 AUTHOR_EMAIL="vincevillamora@gmail.com"          # Author email
-USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"  # Custom user agent string to prevent detection
-LAST_UPD=$(date -u)
-GIT_CREDS=$(cat /opt/scripts/ads/.gittoken)      # Git token in the format of ${USERNAME}:${TOKEN/PASSWORD}
+GIT_CREDS=$(cat "$GIT_CREDS_FILE")      # Git token in the format of ${USERNAME}:${TOKEN/PASSWORD}
 GIT_PROT="https"                                 # Git Protocol to use
 GIT_URL="github.com/vincejv/fireboglists"        # Destination repo, without protocol specified
-DELAY_WGET="4"                                  # Add wait to prevent scraping detection
 
 echo "Setting up git credentials"
 /usr/bin/git config user.name "$AUTHOR_NAME"
@@ -44,11 +65,18 @@ echo "Commiting to repository"
 echo "Pushing to repository"
 /usr/bin/git push "${GIT_PROT}://${GIT_CREDS}@${GIT_URL}"
 
-echo "Wait for git cache..."
-/usr/bin/sleep 5
-echo "Updating pihole lists"
+# Only update if Gravity list is older than DAYS_AGO
+if (( GRAVITY_LAST_UPD <= DAYS_AGO )); then
+  echo "Updating gravity db"
+  echo "Wait for git cache..."
+  /usr/bin/sleep 5
+  echo "Updating pihole lists"
 
-/usr/bin/systemd-notify --status="Updating pihole blocklists"
-/usr/local/bin/pihole -g
+  /usr/bin/systemd-notify --status="Updating pihole blocklists"
+  /usr/local/bin/pihole -g
+else
+  echo "Gravity db was recently updated, skipping update"
+fi
+
 echo "Done!"
 /usr/bin/systemd-notify --status="Done"
